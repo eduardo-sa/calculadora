df027c8c816218b1627d6f7f5b76d3eaca3908bf
eduardo <eduardo.sa@acad.ifma.edu.br>
2019-09-24 15:50:16 -0300

    primeiro envio

---
+++ js/calculadora.js
@@ -0,0 +1,6 @@
+function calcular(){
+	v1 = document.getElementById('valor').value;
+	document.getElementById('primeiroV').value = v1;
+	v2 = document.getElementById('valor').value;
+	document.getElementById('segundoV').value = v2;
+}
